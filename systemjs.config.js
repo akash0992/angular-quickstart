7530f72a1f91a7a113b251e0fa9ca7ef45cc23be
Akash Yadav <akash0992@gmail.com>
2016-10-22 01:42:34 +0530

    code refactored

--- systemjs.config.js
+++ systemjs.config.js
@@ -21,7 +21,7 @@
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
-            app: 'app',
+            app: 'src',
 
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
@@ -44,8 +44,8 @@
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
             app: {
-                main: './main.ts',
-                defaultExtension: 'ts'
+                main: './main.js',
+                defaultExtension: 'js'
             },
             rxjs: {
                 defaultExtension: 'js'
