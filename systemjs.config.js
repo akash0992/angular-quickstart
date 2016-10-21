055ea73d535136c98d11291c1fd56a397d800942
Akash Yadav <akash0992@gmail.com>
2016-10-21 14:04:34 +0530

    demo code for services

--- systemjs.config.js
+++ systemjs.config.js
@@ -4,14 +4,25 @@
  */
 (function (global) {
     System.config({
+        // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+        transpiler: 'ts',
+        typescriptOptions: {
+            tsconfig: true
+        },
+        meta: {
+            'typescript': {
+                "exports": "ts"
+            }
+        },
         paths: {
             // paths serve as alias
-            'npm:': 'node_modules/'
+            'npm:': 'https://unpkg.com/'
         },
         // map tells the System loader where to look for things
         map: {
             // our app is within the app folder
             app: 'app',
+
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,20 +32,25 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+            '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
+
             // other libraries
-            'rxjs':                       'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+            'rxjs':                      'npm:rxjs',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+            'ts':                        'npm:plugin-typescript@4.0.10/lib/plugin.js',
+            'typescript':                'npm:typescript@2.0.2/lib/typescript.js',
+
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
             app: {
-                main: './main.js',
-                defaultExtension: 'js'
+                main: './main.ts',
+                defaultExtension: 'ts'
             },
             rxjs: {
                 defaultExtension: 'js'
             },
-            'angular2-in-memory-web-api': {
+            'angular-in-memory-web-api': {
                 main: './index.js',
                 defaultExtension: 'js'
             }
